e498579b6ca41575d5b52cc80aa63cd004eec78e
François de Metz <francois@2metz.fr>
2021-07-26 13:12:59 +0200

    Filter available levels.

--- src/indoorequal.js
+++ src/indoorequal.js
@@ -1,4 +1,6 @@
+import BaseObject from 'ol/Object';
 import { getLayer } from './layer';
+import findAllLevels from './levels';
 
 /**
  * Load the indoor= source and layers in your map.
@@ -8,14 +10,42 @@ import { getLayer } from './layer';
  * @param {string} [options.apiKey] The API key if you use the default tile URL (get your free key at [indoorequal.com](https://indoorequal.com)).
  * @return {IndoorEqual} `this`
  */
-export default class IndoorEqual {
+export default class IndoorEqual extends BaseObject {
   constructor(map, options = {}) {
     const defaultOpts = { url: 'https://tiles.indoorequal.org/' };
     const opts = { ...defaultOpts, ...options };
     if (opts.url === defaultOpts.url && !opts.apiKey) {
       throw 'You must register your apiKey at https://indoorequal.com before and set it as apiKey param.';
     }
-    const urlParams = opts.apiKey ? `?key=${opts.apiKey}` : '';
-    map.addLayer(getLayer(`${opts.url}${urlParams}`));
+    super({ levels: [] });
+
+    this.map = map;
+    this.url = opts.url;
+    this.apiKey = opts.apiKey;
+
+    this._addLayer();
+  }
+
+  _addLayer() {
+    const urlParams = this.apiKey ? `?key=${this.apiKey}` : '';
+    this.layer = getLayer(`${this.url}${urlParams}`);
+    this.map.addLayer(this.layer);
+
+    this._listenForLevels();
+  }
+
+  _listenForLevels() {
+    this.layer.on('change:source', () => {
+      const source = this.layer.getSource();
+
+      source.on('tileloadend', () => {
+        const extent = this.map.getView().calculateExtent(this.map.getSize());
+        const features = source.getFeaturesInExtent(extent);
+        const areaFeatures = features.filter((feature) => {
+          return feature.getProperties().layer === 'area';
+        });
+        this.set('levels', findAllLevels(areaFeatures));
+      });
+    });
   }
 }
--- test/indoorequal.test.js
+++ test/indoorequal.test.js
@@ -1,10 +1,13 @@
 jest.mock('../src/layer');
 
+import Feature from 'ol/Feature';
 import IndoorEqual, { getLayer } from '../src/';
 
 describe('IndoorEqual', () => {
   it('get the indoorequal layer with the default url', () => {
-    const map = { addLayer: () => {} };
+    const map = { addLayer: jest.fn() };
+    const getLayerReturn = { on: jest.fn() };
+    getLayer.mockReturnValueOnce(getLayerReturn);
     new IndoorEqual(map, { apiKey: 'test' });
     expect(getLayer).toHaveBeenCalledWith('https://tiles.indoorequal.org/?key=test');
   });
@@ -16,15 +19,56 @@ describe('IndoorEqual', () => {
   });
 
   it('get the indoorequal layer with the user defined url', () => {
-    const map = { addLayer: () => {} };
+    const map = { addLayer: jest.fn() };
+    const getLayerReturn = { on: jest.fn() };
+    getLayer.mockReturnValueOnce(getLayerReturn);
     new IndoorEqual(map, { url: 'http://localhost:8090/' });
     expect(getLayer).toHaveBeenCalledWith('http://localhost:8090/');
   });
 
   it('load and add the the indoorequal layer', () => {
-    const addLayerMock = jest.fn();
-    const map = { addLayer: addLayerMock };
+    const map = { addLayer: jest.fn() };
+    const getLayerReturn = { on: jest.fn() };
+    getLayer.mockReturnValueOnce(getLayerReturn);
     new IndoorEqual(map, { apiKey: 'test' });
-    expect(addLayerMock.mock.calls.length).toEqual(1);
+    expect(map.addLayer.mock.calls.length).toEqual(1);
+  });
+
+  it('expose the available levels', (done) => {
+    let tileLoadEndCallback;
+
+    const map = {
+      addLayer: jest.fn(),
+      getView: () => {
+        return {
+          calculateExtent: () => 1
+        };
+      },
+      getSize: jest.fn(),
+    };
+    const getLayerReturn = {
+      on: (_eventName, callback) => callback(),
+      getSource: () => {
+        return {
+          on: (_eventName, callback) => tileLoadEndCallback = callback,
+          getFeaturesInExtent: () => {
+            return [
+              new Feature({ layer: 'area', level: 0 }),
+              new Feature({ layer: 'area', level: 1 }),
+              new Feature({ layer: 'area', level: -2 }),
+              new Feature({ layer: 'poi',  level: 3 }),
+            ];
+          }
+        };
+      }
+    };
+    getLayer.mockReturnValueOnce(getLayerReturn);
+    const indoorequal = new IndoorEqual(map, { apiKey: 'test' });
+    expect(indoorequal.get('levels')).toEqual([]);
+    indoorequal.on('change:levels', (levels) => {
+      expect(indoorequal.get('levels')).toEqual([1, 0, -2]);
+      done();
+    });
+    tileLoadEndCallback();
   });
 });
