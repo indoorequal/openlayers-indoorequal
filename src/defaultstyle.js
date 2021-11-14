d595cc1e5727e0b47383771d7ec9e2916cd0da30
François de Metz <francois@2metz.fr>
2021-11-14 09:51:26 +0100

    Remove uneeded layer check.

--- src/defaultstyle.js
+++ src/defaultstyle.js
@@ -16,16 +16,16 @@ function areaLayer(feature, resolution) {
 
   let stroke;
 
-  if (properties.layer === 'area' && ['area', 'corridor', 'plaform'].includes(properties.class)) {
+  if (['area', 'corridor', 'plaform'].includes(properties.class)) {
     stroke = new Stroke({
       color: '#bfbfbf',
       width: 1
     });
   }
-  if (properties.layer === 'area' && properties.class === 'column') {
+  if (properties.class === 'column') {
     color = '#bfbfbf';
   }
-  if (properties.layer === 'area' && ['room', 'wall'].includes(properties.class)) {
+  if (['room', 'wall'].includes(properties.class)) {
     stroke = new Stroke({
       color: 'gray',
       width: 2
